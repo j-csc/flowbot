cc84c1f6fdb4f16f3cc1844ed56c6e880c659eb1
Jason Chen <superj63@hotmail.com>
2021-01-03 22:49:45 -0800

    asdsad

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def chromeSetup():
 	chrome_opts.add_argument("window-size=1920,1080")
 	chrome_opts.add_argument("--disable-dev-shm-usage")
 	chrome_opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
-	# chrome_opts.add_argument("--headless")
+	chrome_opts.add_argument("--disable-blink-features=AutomationControlled")
 
   # driver = webdriver.Chrome("./chromedriver_linux"ChromeDriverManager().install(),options=chrome_opts)
 	driver = uc.Chrome(options=chrome_opts)
